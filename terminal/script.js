1db5e2bbb3842dbec6106c4f70c7d1dcab837e24
Shaunak G <hello@srg.codes>
2021-06-23 11:34:43 +1000

    add preliminary interactive terminal

---
+++ terminal/script.js
@@ -0,0 +1,34 @@
+
+// Specify custom API with ?useAPI=my-api-host.com
+const apiHost = usp.get("useAPI") || "60api.srg.id.au"
+
+const socket = new WebSocket(
+    `${document.location.protocol === "http:" ? "ws" : "wss"}://${
+      apiHost
+    }/${ path || "ws/_interactive_terminal" }`
+);
+
+const term = new Terminal();
+
+socket.onopen = () => {
+
+    const websocketAddon = new AttachAddon.AttachAddon(socket);
+    const resizeAddon = new FitAddon.FitAddon();
+
+    term.loadAddon(websocketAddon);
+    term.loadAddon(resizeAddon);
+
+    term.open(document.getElementById("terminal"));
+
+    resizeAddon.fit();
+    window.addEventListener("resize", () => resizeAddon.fit());
+
+    socket.addEventListener("message", e => {
+
+        if (e.data == "__TERMEXIT") {
+            return done();
+        }
+
+    })
+
+}
